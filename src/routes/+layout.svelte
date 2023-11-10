561684683f5ffd18ba985f38e84b9661be69976d
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-11-10 15:35:28 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.postcss';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<main class="mx-auto max-w-7xl py-8">
+	<slot />
+</main>
+
+<ModeWatcher />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+A personal playground to learn datavis (data visualization), especially with Svelte and D3.js, but
+also with other tools and libraries.
